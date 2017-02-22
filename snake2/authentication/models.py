24c87830ca081e9e40f816b0c3623145cfa48b91
ViktorBarzin <vbarzin@gmail.com>
2017-02-22 12:52:03 +0200

    applied profile migrations

--- snake2/authentication/models.py
+++ snake2/authentication/models.py
@@ -3,7 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
-# from lobby.models import Lobby
+from lobby.models import Lobby
 # Create your models here.
 
 
+class Profile(User):
+    lobby = models.ForeignKey(Lobby, related_name='users', on_delete=models.PROTECT, null=True, blank=True)
+    lobby_owned = models.OneToOneField(Lobby, related_name='owner', on_delete=models.PROTECT, null=True, blank=True)
