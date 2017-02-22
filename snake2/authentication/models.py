f358449930b2db6a294c9dbd819ec8e7861d20a2
ViktorBarzin <vbarzin@gmail.com>
2017-02-22 14:35:14 +0200

    adding profile models

--- snake2/authentication/models.py
+++ snake2/authentication/models.py
@@ -10,3 +10,14 @@ from lobby.models import Lobby
 class Profile(User):
     lobby = models.ForeignKey(Lobby, related_name='users', on_delete=models.PROTECT, null=True, blank=True)
     lobby_owned = models.OneToOneField(Lobby, related_name='owner', on_delete=models.PROTECT, null=True, blank=True)
+
+    games_won = models.IntegerField(default=0)
+    games_played = models.IntegerField(default=0)
+    win_ratio = models.FloatField(default=0)
+    # date_joined = models.DateTimeField()
+    profile_picture_url = models.CharField(max_length=255,null=True)
+
+    def get_win_ratio(self):
+        if games_played == 0:
+            return 0
+        return games_won / games_played
