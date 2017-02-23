6d8b813cdda755f0fb826d966eb2aab5122d6882
Jor0Stam <sonik333@mail.bg>
2017-02-23 23:03:47 +0200

    test commit

--- snake2/game/views.py
+++ snake2/game/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+
 # Create your views here.
 
 # global variable which is used for ocmmunication between client and snake
