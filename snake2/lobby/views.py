9e9d10b32712968fa3958114da51b823e720112b
ViktorBarzin <vbarzin@gmail.com>
2017-02-23 13:33:58 +0200

    updating users in lobby done

--- snake2/lobby/views.py
+++ snake2/lobby/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, reverse, redirect
-from django.http import HttpResponseRedirect, Http404, HttpResponseForbidden
+from django.http import HttpResponse, HttpResponseRedirect, Http404, HttpResponseForbidden, HttpResponseBadRequest
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.contrib.auth.models import User
+# from django.utils import simplejson
+from django.core import serializers
+import json
+
 
 from lobby.models import Lobby
 # Create your views here.
@@ -29,12 +33,13 @@ def index(request, lobby_id):
 def create_lobby(request):
     user = request.user
     try:
-        import ipdb; ipdb.set_trace()# BREAKPOINT)
+        # lobby = Lobby.objects.create(is_full=False, owner=user.profile, users = [user.profile])
         lobby = Lobby.objects.create(is_full=False, owner=user.profile)
-        lobby.save()
+        import ipdb; ipdb.set_trace()# BREAKPOINT)
         # Check if user is already in lobby
         join_lobby(user, lobby)
 
+        lobby.save()
         # user.save()
     except IntegrityError:
        # return HttpResponseForbidden('You are not allowed to create more than 1 lobby!')
@@ -49,20 +54,18 @@ def create_lobby(request):
         return redirect('/lobby/{}'.format(lobby.id))
 
 
+# This is actually when user leaves lobby, not neccesasri delete lobby
 @login_required(login_url='login')
-def delete_lobby(request, lobby_id):
+def leave_lobby_view(request, lobby_id):
+    import ipdb; ipdb.set_trace()
+    user = request.user
     lobby = Lobby.objects.filter(id=lobby_id).first()
-    if lobby is not None:
-        import ipdb; ipdb.set_trace()
-        if request.user.profile not in lobby.users.all():
-            raise ValueError('Cannot remove this user form the lobby because he has not joined it!')
-        else:
-            leave_lobby(request.user, lobby)
-        if len(lobby.users.all()) == 0:
-            lobby.delete()
+    leave_lobby(user, lobby)
+    # if lobby is not None:
+    #     lobby.delete()
 
-    else:
-        raise ValueError('There is no lobby with id "{}"'.format(lobby.id))
+    # else:
+    #     raise ValueError('There is no lobby with id "{}"'.format(lobby.id))
     return HttpResponseRedirect(reverse('baseapp:index'))
 
 
@@ -72,7 +75,6 @@ def invite_friends(request, lobby_id):
     pass
 
 
-# @login_required(login_url='login')
 def join_lobby(user, lobby):
     import ipdb; ipdb.set_trace()
 
@@ -83,6 +85,41 @@ def join_lobby(user, lobby):
 
 def leave_lobby(user, lobby):
     if user.profile in lobby.users.all():
-        import ipdb; ipdb.set_trace()
+        if user.profile not in lobby.users.all():
+            raise ValueError('Cannot remove this user form the lobby because he has not joined it!')
+        else:
+            import ipdb; ipdb.set_trace()
 
-        lobby.users.remove(user.profile)
+            lobby.users.remove(user.profile)
+
+        if len(lobby.users.all()) == 0:
+            lobby.delete()
+
+
+def get_users_in_lobby_ajax(request):
+    lobby_id = request.GET.get('lobby_id')
+    if not lobby_id:
+        return HttpResponseBadRequest
+    lobby_id = int(lobby_id)
+    lobby = Lobby.objects.filter(id = lobby_id).first()
+
+    # import ipdb; ipdb.set_trace()# BREAKPOINT)
+    # have to append username manually because it is in the django User model
+    # and lobby.users are my custom user model
+    users = lobby.users.all()
+    # for user in users:
+    #     user.set_field({'profile_username': user.username})
+    usernames = [{"user":x.username}  for x in lobby.users.all()]
+    # [{user:"username"}, {user:"username"}]
+    # data = serializers.serialize('json', usernames)
+    data = json.dumps(usernames)
+    # data = []
+    # if len(usernames) == 1:
+    #     data.append(usernames)
+    # else:
+    #     for username in usernames:
+    #         data.append(username)
+    # data = json.dumps(usernames)
+    # return HttpResponse(simplejson.dumps({'foo':'bar'}), mimetype='application/json')
+    return HttpResponse(data)
+
